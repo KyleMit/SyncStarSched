f58c1deac9022f9249f5b799c182192f1c86c731
KyleMit <kylemit@gmail.com>
2019-01-08 23:49:12 -0500

    standardize datetime in logs, refactor code sequentially, update launch config

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const config = require('./config.json');
 const secrets = require('./secrets.json');
 
 const colors = {
+    Bright: "\x1b[1m"  + "%s" + "\x1b[0m",
+    Dim:    "\x1b[2m"  + "%s" + "\x1b[0m",
     Black:  "\x1b[30m" + "%s" + "\x1b[0m",
     White:  "\x1b[37m" + "%s" + "\x1b[0m",
     Red:    "\x1b[31m" + "%s" + "\x1b[0m",
@@ -15,42 +17,42 @@ const colors = {
     Blue:   "\x1b[34m" + "%s" + "\x1b[0m",
 };
 
+const defaultEvent = {  
+    'summary': 'Starbucks Shift',
+    'location': '49 Church St #2072, Burlington, VT 05401',
+    'description': `Automatically added from ${config.urls.starbucks}.`,
+    'start': {
+      'dateTime': '2019-01-01T12:00:00-05:00',
+      'timeZone': 'America/New_York'
+    },
+    'end': {
+      'dateTime': '2019-01-01T17:00:00-05:00',
+      'timeZone': 'America/New_York'
+    },
+    'reminders': {
+      'useDefault': false,
+      'overrides': [
+        {'method': 'popup', 'minutes': 60 * 4},
+        {'method': 'popup', 'minutes': 60},
+        {'method': 'popup', 'minutes': 15}
+      ]
+    }
+};
+
 // launch program with async iife
 (async () => {
 
-    const oAuth2Client = await getOAuthClient()
-    
-    const schedule = await scrapeSite()
+    //const schedule = await scrapeStarbucks()
 
-    await syncSchedule(oAuth2Client, schedule)
+    const calendarApi = await getCalendarApi()
+    
+    const upcomingEvents = await getCalendarEvents(calendarApi)
+
+    await syncSchedule(calendarApi, schedule, upcomingEvents)
 
 })()
 
-async function getOAuthClient() {
-    // get developer API from credentials.json file
-    const credResponse = await readJsonAsync(config.paths.creds)
-    if (credResponse.err) console.error(`Error loading file ${credPath}`);
-    
-    // destructure credentials
-    const credentials = credResponse.data
-    const { client_secret, client_id, redirect_uris } = credentials.installed;
-
-    // setup oAuth Client
-    const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
-
-    // get user permission from token.json file
-    const tokenResponse = await readJsonAsync(config.paths.token)
-
-    // if we don't have a token file, create one
-    const token = tokenResponse.err ? await getAccessToken(oAuth2Client) : tokenResponse.data;
-
-    // authorize client for use    
-    oAuth2Client.setCredentials(token);
-
-    return oAuth2Client
-}
-
-async function scrapeSite() {
+async function scrapeStarbucks() {
     // open browse and navigate to page
     const browser = await puppeteer.launch({headless: true, devtools: false}); // update config in prod
     const page = await browser.newPage();
@@ -118,6 +120,21 @@ async function scrapeSite() {
          return shifts;
     })
 
+    // transform retrieved schedules
+    for (let i=0; i < webShifts.length; i++) {
+        const shift = webShifts[i];
+
+        // upcoming shift year is current execution year unless we're in dec and shift is in jan
+        shift.year = (new Date()).getFullYear()
+        if (new Date().getMonth() == 11 && shift.day.includes("Jan")) shift.year++
+
+        shift.startDateTime = `${shift.day.split(",")[1]} ${shift.year}, ${shift.shiftStart}`
+        shift.endDateTime = `${shift.day.split(",")[1]} ${shift.year}, ${shift.shiftEnd}`
+        shift.startMoment = moment(shift.startDateTime, "MMMM D YYYY, hh:mm A")
+        shift.endMoment = moment(shift.endDateTime, "MMMM D YYYY, hh:mm A")
+        shift.duration = moment.duration(shift.endMoment.diff(shift.startMoment));
+    }     
+
     // var sampleShift = {
     //     "storeNumber": "007629",
     //     "storeName": "Burlington Town Center",
@@ -129,7 +146,8 @@ async function scrapeSite() {
     // log events for fun
     console.log(colors.Yellow, `Retrieved ${webShifts.length} shift(s) from Starbucks:`)
     webShifts.forEach(shift => {
-        console.log(`Shift: ${shift.day}, ${shift.shiftStart}`);
+        console.log(shift.startMoment > moment() ? colors.Bright : colors.White,
+                    `Shift: ${shift.startMoment.format("ddd, MMM D, h:mma")}`);
     });
     console.log("\r\n")
 
@@ -138,9 +156,8 @@ async function scrapeSite() {
 
     return webShifts
 }
-
-// determine what page we're on
 async function getPageStatus(myPage) {
+    // determine what page we're on
     return await myPage.evaluate(() => {
             
         const partner = document.querySelectorAll(".txtUserid")
@@ -161,12 +178,60 @@ async function getPageStatus(myPage) {
     })
 }
 
-async function syncSchedule(auth, schedule) {
-    
-    // init google api library
-    const calendar = google.calendar({ version: 'v3', auth });
 
-    const calendarId = await getCalendarId(calendar, "Starbucks")
+async function getCalendarApi() {
+    const oAuth2Client = await getOAuthClient()
+    
+    const calendar = google.calendar({ version: 'v3', oAuth2Client });
+
+    return calendar
+}
+async function getOAuthClient() {
+    // get developer API from credentials.json file
+    const credResponse = await readJsonAsync(config.paths.creds)
+    if (credResponse.err) console.error(`Error loading file ${credPath}`);
+    
+    // destructure credentials
+    const credentials = credResponse.data
+    const { client_secret, client_id, redirect_uris } = credentials.installed;
+
+    // setup oAuth Client
+    const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
+
+    // get user permission from token.json file
+    const tokenResponse = await readJsonAsync(config.paths.token)
+
+    // if we don't have a token file, create one
+    const token = tokenResponse.err ? await getAccessToken(oAuth2Client) : tokenResponse.data;
+
+    // authorize client for use    
+    oAuth2Client.setCredentials(token);
+
+    return oAuth2Client
+}
+async function getAccessToken(oAuth2Client) {
+
+    // prompt for online authorization
+    const authUrl = oAuth2Client.generateAuthUrl({access_type: 'offline', scope: config.scope});
+    console.log('Authorize this app by visiting this url:', authUrl);
+
+    // get response from user
+    const authCode = await readlineAskAsync('Enter the code from that page here: ')
+
+    // get token from auth client
+    const tokenResponse = await oAuth2Client.getToken(authCode)
+    if (tokenResponse.res.statusText != "OK") console.error("Could not retrieve token")
+
+    // Store the token to disk for later program executions
+    await writeJsonAsync(config.paths.token, tokenResponse.tokens)
+
+    return tokenResponse.tokens;
+}
+
+
+async function getCalendarEvents(calendar) {
+
+    const calendarId = secrets.cal.id || await getCalendarId(calendar, secrets.cal.name)
 
     // check current events
     const eventsRes = await calendar.events.list({
@@ -179,48 +244,36 @@ async function syncSchedule(auth, schedule) {
     
     const upcomingEvents = eventsRes.data.items;
 
-    // log events for fun
+    // log events 
     console.log(colors.Yellow, `Retrieved ${upcomingEvents.length} upcoming calendar appointment(s):`)
     upcomingEvents.forEach(event => {
-        console.log(`Event: ${event.start.dateTime || event.start.date} - ${event.summary}`);
+        console.log(`Event: ${moment(event.start.dateTime).format("ddd, MMM D, h:mma")}`);
     });
     console.log("\r\n")
 
-    const defaultEvent = {  
-        'summary': 'Starbucks Shift',
-        'location': '49 Church St #2072, Burlington, VT 05401',
-        'description': 'Automatically added from https://mysite.starbucks.com/MySchedule/Schedule.aspx.',
-        'start': {
-          'dateTime': '2019-01-01T12:00:00-05:00',
-          'timeZone': 'America/New_York'
-        },
-        'end': {
-          'dateTime': '2019-01-01T17:00:00-05:00',
-          'timeZone': 'America/New_York'
-        },
-        'reminders': {
-          'useDefault': false,
-          'overrides': [
-            {'method': 'popup', 'minutes': 60 * 4},
-            {'method': 'popup', 'minutes': 60},
-            {'method': 'popup', 'minutes': 15}
-          ]
-        }
-    }
+    return upcomingEvents;
+}
+async function getCalendarId(calendar, calendarName) {
+try {
+     // call all active calendars to look for this one
+     const calRes = await calendar.calendarList.list({})
+     const calendars = calRes.data.items;
+     const myCal = calendars.filter(cal => cal.summary == calendarName)[0];
+     const calendarId = myCal.id; // "60m640rj25mngq7m57j0hbg518@group.calendar.google.com"
+ 
+     return calendarId;
+} catch (error) {
+    console.log(error)
+}
+
+}
+
+
+async function syncSchedule(calendar, schedule, upcomingEvents) {
 
     // find new shifts
     for (let i=0; i < schedule.length; i++) {
         const shift = schedule[i];
-
-        // upcoming shift year is current execution year unless we're in dec and shift is in jan
-        shift.year = (new Date()).getFullYear()
-        if (new Date().getMonth() == 11 && shift.day.includes("Jan")) shift.year++
-
-        shift.startDateTime = `${shift.day.split(",")[1]} ${shift.year}, ${shift.shiftStart}`
-        shift.endDateTime = `${shift.day.split(",")[1]} ${shift.year}, ${shift.shiftEnd}`
-        shift.startMoment = moment(shift.startDateTime, "MMMM D YYYY, hh:mm A")
-        shift.endMoment = moment(shift.endDateTime, "MMMM D YYYY, hh:mm A")
-        shift.duration = moment.duration(shift.endMoment.diff(shift.startMoment));
 
         var matchedEvent = upcomingEvents.some(evt => {
             return moment(evt.start.dateTime).format() == shift.startMoment.format() &&
@@ -228,36 +281,30 @@ async function syncSchedule(auth, schedule) {
         })
 
         shift.isNew = !matchedEvent
-
         shift.inFuture = shift.startMoment > moment()
-
         shift.shouldInsert = shift.isNew && shift.inFuture
-     
     }
     
     // insert new shifts
     var insertShifts = schedule.filter(s=> s.shouldInsert)
     console.log(colors.Green, `Inserting ${insertShifts.length} new shift(s):`)
 
-    for (let i=0; i < schedule.length; i++) {
-        const shift = schedule[i];
+    for (let i=0; i < insertShifts.length; i++) {
+        const shift = insertShifts[i];
 
-        if (shift.isNew && shift.inFuture) {
+        let insertShift = Object.assign({}, defaultEvent)
+        insertShift.start.dateTime = shift.startMoment.format()
+        insertShift.end.dateTime = shift.endMoment.format()
+        insertShift.summary = `(${shift.duration.asHours()}HR) Starbucks`
+        insertShift.location = `${shift.storeName} - #${shift.storeNumber}`
 
-            let insertShift = Object.assign({}, defaultEvent)
-            insertShift.start.dateTime = shift.startMoment.format()
-            insertShift.end.dateTime = shift.endMoment.format()
-            insertShift.summary = `(${shift.duration.asHours()}HR) Starbucks`
-            insertShift.location = `${shift.storeName} - #${shift.storeNumber}`
+        let insertRes = await calendar.events.insert({
+            calendarId: calendarId,
+            requestBody: insertShift,
+        })
 
-            let insertRes = await calendar.events.insert({
-                calendarId: calendarId,
-                requestBody: insertShift,
-            })
-
-            console.log(`Inserted: ${shift.day}, ${shift.start}`);
-            // todo - send update if requested
-        }
+        console.log(`Inserted: ${shift.startMoment.format("ddd, MMM D, h:mma")}`);
+        // todo - send update if requested
     }
     console.log("\r\n")
 
@@ -287,45 +334,11 @@ async function syncSchedule(auth, schedule) {
             eventId: evt.id,
         })
 
-        console.log(`Deleted: ${evt.start.dateTime}`);
+        console.log(`Deleted: ${moment(evt.start.dateTime).format("ddd, MMM D, h:mma")}`);
     }
 
 }
 
-async function getCalendarId(calendar, calendarName) {
-
-    // call all active calendars to look for this one
-    const calRes = await calendar.calendarList.list({})
-    const calendars = calRes.data.items;
-    const myCal = calendars.filter(cal => cal.summary == calendarName)[0];
-    const calendarId = myCal.id; // "60m640rj25mngq7m57j0hbg518@group.calendar.google.com"
-
-    return calendarId;
-}
-
-/**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
- */
-async function getAccessToken(oAuth2Client) {
-
-    // prompt for online authorization
-    const authUrl = oAuth2Client.generateAuthUrl({access_type: 'offline', scope: config.scope});
-    console.log('Authorize this app by visiting this url:', authUrl);
-
-    // get response from user
-    const authCode = await readlineAskAsync('Enter the code from that page here: ')
-
-    // get token from auth client
-    const tokenResponse = await oAuth2Client.getToken(authCode)
-    if (tokenResponse.res.statusText != "OK") console.error("Could not retrieve token")
-
-    // Store the token to disk for later program executions
-    await writeJsonAsync(config.paths.token, tokenResponse.tokens)
-
-    return tokenResponse.tokens;
-}
 
 function readJsonAsync(filePath) {
     return new Promise(
@@ -339,8 +352,7 @@ function readJsonAsync(filePath) {
             })
        }
      );
-};
-
+}
 function writeJsonAsync(filePath, obj) {
     return new Promise(
         (resolve) => {
@@ -350,8 +362,7 @@ function writeJsonAsync(filePath, obj) {
             })
        }
      );
-};
-
+}
 function readlineAskAsync(question) {
     return new Promise(
         (resolve) => {
